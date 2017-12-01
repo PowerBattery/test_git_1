ecbecd724ee1d299d8cfb57f8271138d0e058e50
HJ K <bdt02162@naver.com>
2017-12-01 19:53:01 +0900

    solve

--- hello.c
+++ hello.c
@@ -2,11 +2,8 @@
 
 int main(void)
 {
-<<<<<<< HEAD
   printf("123213123\n"); 
-=======
   printf("git study!\n");
->>>>>>> origin/master
   printf("hello world!\n");
   printf("hello!\n");
   printf("KimDonghyeon\n");
